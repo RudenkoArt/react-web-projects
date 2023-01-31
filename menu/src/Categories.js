879a24dd21513c85e075acb3e885f9a9c27badbd
RudenkoArt <rudnkoart@gmail.com>
2023-01-31 11:41:58 +0200

    ready to deploy menu

--- menu/src/Categories.js
+++ menu/src/Categories.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import items from './data';
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({ categories, filterItems }) => {
+  const renderedButtons = categories.map((category, index) => {
+    return (
+      <button
+        key={index}
+        type='button'
+        className='filter-btn'
+        onClick={() => filterItems(category)}
+      >
+        {category}
+      </button>
+    );
+  });
+
+  return <div className='btn-container'>{renderedButtons}</div>;
 };
 
 export default Categories;
