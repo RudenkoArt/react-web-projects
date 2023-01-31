b21781ce0ca2844a7194ed57dcd333fc789af737
RudenkoArt <rudnkoart@gmail.com>
2023-01-31 10:59:57 +0200

    ready to deploy accordion

--- accordion/src/App.js
+++ accordion/src/App.js
@@ -1,16 +1,23 @@
 import React, { useState } from 'react';
 import data from './data';
-
 import SingleQuestion from './Question';
 
 function App() {
-  const renderedQuestions = () => {
-    return data.map((question) => {
-      return <SingleQuestion {...question} />;
-    });
-  };
+  const [questions, setQuestions] = useState(data);
 
-  return <article>{renderedQuestions()}</article>;
+  const renderedQuestions = () =>
+    questions.map((question) => {
+      return <SingleQuestion key={question.id} {...question} />;
+    });
+
+  return (
+    <main>
+      <div className='container'>
+        <h3>questions and answers about login</h3>
+        <section className='info'> {renderedQuestions()}</section>
+      </div>
+    </main>
+  );
 }
 
 export default App;
