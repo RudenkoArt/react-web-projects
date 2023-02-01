989509268d1dc6ee670ea2bab5db102e44ebc023
RudenkoArt <rudnkoart@gmail.com>
2023-02-01 12:15:22 +0200

    ready to deploy grocery-bud

--- grocery-bud/src/App.js
+++ grocery-bud/src/App.js
@@ -1,9 +1,113 @@
-import React, { useState, useEffect } from 'react'
-import List from './List'
-import Alert from './Alert'
+import React, { useState, useEffect, useRef } from 'react';
+import List from './List';
+import Alert from './Alert';
 
-function App() {
-  return <h2>grocery bud setup</h2>
-}
+const getLocalStorage = () => {
+  let list = localStorage.getItem('list');
 
-export default App
+  if (list) {
+    return JSON.parse(list);
+  } else {
+    return [];
+  }
+};
+
+const App = () => {
+  const [name, setName] = useState('');
+  const [list, setList] = useState(getLocalStorage());
+  const [isEditing, setIsEditing] = useState(false);
+  const [editId, setEditId] = useState(null);
+  const [alert, setAlert] = useState({
+    show: false,
+    msg: '',
+    type: '',
+  });
+
+  useEffect(() => {
+    localStorage.setItem('list', JSON.stringify(list));
+  }, [list]);
+
+  const inputRef = useRef();
+
+  const showAlert = (show = false, msg = '', type = '') => {
+    return setAlert({ show, msg, type });
+  };
+
+  const clearList = () => {
+    showAlert(true, 'empty list', 'danger');
+    setList([]);
+  };
+
+  const editItem = (id) => {
+    const itemToEdit = list.find((item) => item.id === id);
+    setIsEditing(true);
+    setEditId(id);
+    setName(itemToEdit.title);
+    inputRef.current.focus();
+  };
+
+  const removeItem = (id) => {
+    showAlert(true, 'item removed', 'danger');
+    const newList = list.filter((item) => item.id !== id);
+    setList(newList);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!name) {
+      showAlert(true, 'please enter value', 'danger');
+    } else if (name && isEditing) {
+      setList(
+        list.map((item) => {
+          if (item.id === editId) {
+            return { ...item, title: name };
+          }
+          return item;
+        })
+      );
+      setName('');
+      setEditId(null);
+      setIsEditing(false);
+      showAlert(true, 'value changed', 'success');
+    } else {
+      showAlert(true, 'item added to the list', 'success');
+      const newItem = { id: new Date().getTime().toString(), title: name };
+      setList([...list, newItem]);
+      setName('');
+    }
+  };
+
+  return (
+    <section className='section-center'>
+      <form className='grocery-form' onSubmit={handleSubmit}>
+        {alert.show && <Alert {...alert} removeAlert={showAlert} />}
+        <h3>grocery bud</h3>
+        <div className='form-control'>
+          <input
+            ref={inputRef}
+            type='text'
+            className='grocery'
+            placeholder='e.g. eggs'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            autoFocus
+          />
+          <button type='submit' className='submit-btn'>
+            {isEditing ? 'edit' : 'submit'}
+          </button>
+        </div>
+      </form>
+      {list.length > 0 && (
+        <div className='grocery-container'>
+          <List items={list} removeItem={removeItem} editItem={editItem} />
+          <button className='clear-btn' onClick={clearList}>
+            clear items
+          </button>
+        </div>
+      )}
+    </section>
+  );
+};
+
+export default App;
