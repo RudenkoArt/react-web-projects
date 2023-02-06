9c2a6b27365714ce309cd4246eeb655109a66d6d
RudenkoArt <rudnkoart@gmail.com>
2023-02-06 13:17:35 +0200

    ready to deploy stripe-submenus

--- stripe-submenus/src/context.js
+++ stripe-submenus/src/context.js
@@ -5,9 +5,14 @@ export const AppContext = React.createContext();
 
 export const AppProvider = ({ children }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-  const [isSubmenuOpen, setIsSubmenuOpen] = useState(true);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+  const [location, setLocation] = useState({});
+  const [page, setPage] = useState({ page: '', links: [] });
 
-  const openSubmenu = () => {
+  const openSubmenu = (text, coordinates) => {
+    const page = sublinks.find((link) => link.page === text);
+    setPage(page);
+    setLocation(coordinates);
     setIsSubmenuOpen(true);
   };
 
@@ -32,6 +37,8 @@ export const AppProvider = ({ children }) => {
         isSidebarOpen,
         openSidebar,
         closeSidebar,
+        location,
+        page,
       }}
     >
       {children}
